6ba0b70d0eda15747b0599cf0d96f9e6a43ea953
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2022-11-28 23:41:36 -0800

    savepoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,38 @@
-import { AppShell, Divider, Grid, Group } from "@mantine/core";
+import { Grid } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
+import CanvasHeader from "./components/CanvasHeader";
+import CanvasView from "./components/CanvasView";
 import FileView from "./components/FileView";
 import HeaderWithPath from "./components/HeaderWithPath";
-import Sidebar from "./components/Sidebar";
 import SpotlightContext from "./context/SpotlightContext";
 import "./MenuItem/contextMenuHandler";
 import useContextEvents from "./MenuItem/contextMenuHandler";
 import "./styles.css";
-import CanvasView from "./components/CanvasView";
-import CanvasHeader from "./components/CanvasHeader";
+
+export const CANVAS_TEST = false;
+
 const App: React.FC = () => {
   useContextEvents();
   return (
     <NotificationsProvider>
       <SpotlightContext>
         <Grid columns={2} style={{ height: "100%" }}>
-          <Grid.Col style={{ borderRight: "1px solid black" }} span={1}>
-            {/* <AppShell
-              styles={(theme) => ({
-                main: {
-                  backgroundColor:
-                    theme.colorScheme === "dark"
-                      ? theme.colors.dark[8]
-                      : theme.colors.gray[0],
-                },
-              })}
-            > */}
+          <Grid.Col
+            style={{ borderRight: "1px solid black" }}
+            span={CANVAS_TEST ? 2 : 1}
+          >
             <CanvasHeader />
             <CanvasView />
-            {/* </AppShell> */}
           </Grid.Col>
-          {/* <Divider orientation="vertical" /> */}
-          <Grid.Col
-            span={1}
-            style={{ borderLeft: "1px solid black", height: "100%" }}
-          >
-            {/* <AppShell
-              styles={(theme) => ({
-                main: {
-                  backgroundColor:
-                    theme.colorScheme === "dark"
-                      ? theme.colors.dark[8]
-                      : theme.colors.gray[0],
-                },
-              })}
-            > */}
-            <HeaderWithPath />
-            <FileView />
-            {/* </AppShell> */}
-          </Grid.Col>
+          {!CANVAS_TEST && (
+            <Grid.Col
+              span={1}
+              style={{ borderLeft: "1px solid black", height: "100%" }}
+            >
+              <HeaderWithPath />
+              <FileView />
+            </Grid.Col>
+          )}
         </Grid>
       </SpotlightContext>
     </NotificationsProvider>
